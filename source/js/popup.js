07c571ca76f56c86c85400fdb02bb45d960f40ad
Anya Vigolajnen <avi26.job@yandex.ru>
2019-04-11 05:30:57 +0300

    модалка + меню в МВ

--- source/js/popup.js
+++ source/js/popup.js
@@ -4,6 +4,7 @@ var popupOverlay = document.querySelector('.popup-overlay');
 var popupBtnClose = document.querySelector('.popup-close');
 var wrapper = document.querySelector('.wrapper');
 var popupOpen = document.querySelector('[data-id=js-popup-btn]');
+var popupOpenmodal = document.querySelector('[data-id=js-popup-btn-mob]');
 
 // var popupForm = popup.querySelector('form');
 var inputName = popup.querySelector('[id=name]');
@@ -20,7 +21,21 @@ popupBtnClose.addEventListener('click', function (evt) {
   evt.preventDefault();
   popupOverlay.classList.remove('popup-show');
   document.querySelector('body').classList.remove('overlay');
+
+  document.querySelector(".navigation").classList.add("navigation--closed");
+
+  document.querySelector('body').style.overflow = 'visible';
+  document.querySelector('.paranja').style.position = 'relative';
+  document.querySelector('.page-header__right').style.opacity = '1';
 });
 
+
+popupOpenmodal.addEventListener('click', function (evt) {
+  document.querySelector(".navigation").classList.remove("navigation--opened");
+  
+  popupOverlay.classList.add('popup-show');
+  document.querySelector('body').classList.add('overlay');
+  inputName.focus();
+});
 
 
